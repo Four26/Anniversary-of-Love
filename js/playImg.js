e4b02d5e599bd1b917470574be726f96bacb400d
Four26 <franklinabonero28@gmail.com>
2026-09-26 01:11:13 +0800

    refactor image handling and improve preload functionality

--- js/playImg.js
+++ js/playImg.js
@@ -1,21 +1,18 @@
-
 var btn = document.getElementById("heartTxt");
 btn.style.opacity = 0;
 var btnVal = 0;
 
-function showImage(){
-	//document.getElementById("imgTxt").style.opacity = 0;
+function showImage() {
 	myImage.setAttribute("src", imageArray[imageIndex]);
 	myTxt.innerHTML = txtArray[imageIndex];
-	//document.getElementById("imgTxt").style.opacity = 1 - flag;
 	imageIndex++;
-	if(imageIndex >= len){
+	if (imageIndex >= len) {
 		imageIndex = 0;
 	}
 }
 
-function play(){
-	if(t == 0){
+function play() {
+	if (t == 0) {
 		myImage.setAttribute("src", imageArray[0]);
 		myTxt.innerHTML = txtArray[0];
 		imageIndex = 0;
@@ -24,44 +21,41 @@ function play(){
 	flag = 1 - flag;
 	document.getElementById("typeDiv").style.opacity = flag;
 	document.getElementById("imgTxt").style.opacity = 1 - flag;
-	if(t == 0){
-		//setTimeout(showImage, 1000);
+	if (t == 0) {
 		setInterval(showImage, 2500);
 	}
 	t++;
 }
 
-function preshowImage(){
+function preshowImage() {
 	document.getElementById("imgTxt").style.opacity = 0;
 	myImage.setAttribute("src", imageArray[imageIndex]);
 	myTxt.innerHTML = txtArray[imageIndex];
 	imageIndex++;
-	if(imageIndex >= len){
+	if (imageIndex >= len) {
 		imageIndex = 0;
 	}
 }
 
-function buttonFadeIn(){
-	if(btnVal < 1){
+function buttonFadeIn() {
+	if (btnVal < 1) {
 		btnVal += 0.025;
 		btn.style.opacity = btnVal;
-	}
-	else{
+	} else {
 		clearInterval(buttonInterval);
-		if(ok == 3){
+		if (ok == 3) {
 			ok += 1;
 		}
 	}
 }
 
-// NEW: preload every image before the fast cycling loop starts
-function preloadImages(callback){
+function preloadImages(callback) {
 	var loadedCount = 0;
-	imageArray.forEach(function(src){
+	imageArray.forEach(function (src) {
 		var img = new Image();
-		img.onload = img.onerror = function(){
+		img.onload = img.onerror = function () {
 			loadedCount++;
-			if(loadedCount === imageArray.length){
+			if (loadedCount === imageArray.length) {
 				callback();
 			}
 		};
@@ -69,15 +63,14 @@ function preloadImages(callback){
 	});
 }
 
-
-
-function event(){
-
+function event() {
 	showImageInterval = setInterval(preshowImage, 100);
 
-	imgInterval = setInterval(function (){
-		if(ok == 3){
-			setTimeout(function(){buttonInterval = setInterval(buttonFadeIn, 50);}, 1500);
+	imgInterval = setInterval(function () {
+		if (ok == 3) {
+			setTimeout(function () {
+				buttonInterval = setInterval(buttonFadeIn, 50);
+			}, 1500);
 			clearInterval(imgInterval);
 		}
 	}, 50);
@@ -87,4 +80,4 @@ var showImageInterval;
 var imgInterval;
 var buttonInterval;
 
-preloadImages(event);   // ← wait for all images to load, THEN start the flash loop
+preloadImages(event);
